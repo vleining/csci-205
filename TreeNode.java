22323f74eab46c6b57330c64c3f8507d7636abcd
Verne E. Leininger <vleining@cs.bridgewater.edu>
2018-02-12 13:40:31 -0500

    2/12/2018

---
+++ TreeNode.java
@@ -0,0 +1,30 @@
+public class TreeNode{
+	private int data;
+	private TreeNode left;
+	private TreeNode right;
+
+	public TreeNode (int data){
+		this.data = data;
+		left = null;
+		right = null;
+	}
+
+	public int getData () {
+		return data;
+	}
+	public void setData (int data){
+		this.data = data;
+	}
+	public TreeNode getLeft(){
+		return left;
+	}
+	public void setLeft(TreeNode left){
+		this.left = left;
+	}
+	public TreeNode getRight(){
+		return right;
+	}
+	public void setRight(TreeNode right){
+		this.right = right;
+	}
+}
